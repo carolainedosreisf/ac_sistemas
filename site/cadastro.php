89ff978fc068a52e48c065bfcc1b9e24f0e61e48
Carolaine <carol_dos_reis@hotmail.com>
2021-10-14 23:29:02 -0300

    14/10/2021 parte 2

--- site/cadastro.php
+++ site/cadastro.php
@@ -3,7 +3,6 @@
 <?php include 'header.php' ?>
 <?php include 'capa.php' ?>
 
-<!-- CONTENT =============================-->
 <section class="item content" style="margin-top:50px;margin-bottom:50px;" ng-app="appCadastro" ng-controller="cadastroController">
     <div class="container toparea">
         <div class="underlined-title">
@@ -20,7 +19,17 @@
         </div>
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
-                <form method="post" action="contact.php" id="contactform">
+                <div class="alert alert-danger" role="alert" ng-show="form_cadastro.nr_cpf.$invalid && (form_cadastro.$submitted || form_cadastro.nr_cpf.$dirty)">
+                    CPF Inválido!
+                </div>
+                <div class="alert alert-danger" role="alert" ng-show="cad.senha != cad.confirm_senha && form_cadastro.$submitted">
+                    As senhas não correspondem!
+                </div>
+                <div class="alert alert-danger" role="alert" ng-show="form_cadastro.$invalid && form_cadastro.$submitted">
+                    Preencha os campos destacados!
+                </div>
+                <form id="contactform" name="form_cadastro" novalidate
+                ng-submit="setCadastro()">
                     <div class="form">
                         <div class="painel">
                             <div class="titulo-painel">
@@ -30,24 +39,24 @@
 
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        <label for="nome">Nome:</label>
-                                        <input type="text" name="nome" autocomplete="off">
+                                        <label for="nm_cadastro">Nome:</label>
+                                        <input type="text" name="nm_cadastro" ng-model="cad.nm_cadastro" autocomplete="off" ng-required="true" maxlength="50">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6" ng-class="form_cadastro.nr_cpf.$invalid && (form_cadastro.$submitted || form_cadastro.nr_cpf.$dirty)?'has-error':''">
+                                        <label for="nr_cpf">CPF:</label>
+                                        <input type="text" name="nr_cpf" ng-model="cad.nr_cpf" ui-br-cpf-mask autocomplete="off" ng-required="true">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label for="nr_rg">RG:</label>
+                                        <input type="text" name="nr_rg" autocomplete="off" ng-model="cad.nr_rg" ng-required="true" maxlength="9">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-6">
-                                        <label for="cpf">CPF:</label>
-                                        <input type="text" name="cpf" ng-model="cad.cpf" ui-br-cpf-mask autocomplete="off">
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <label for="rg">RG:</label>
-                                        <input type="text" name="rg" autocomplete="off">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <label for="dt_nascimento">Data Nascimento:</label>
-                                        <input type="text" data-provide="datepicker" class="date_picker" name="dt_nascimento" autocomplete="off" data-date-format="dd/mm/yyyy">
+                                        <label for="dt_nascto">Data Nascimento:</label>
+                                        <input type="text" data-provide="datepicker" class="date_picker" name="dt_nascto" autocomplete="off" data-date-format="dd/mm/yyyy" ng-model="cad.dt_nascto" ng-required="true">
                                     </div>
                                     <div class="col-sm-6">
                                         <label for="sexo">Sexo:</label>
@@ -59,12 +68,12 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-6">
-                                        <label for="telefone">Telefone:</label>
-                                        <input type="text" id="telefone" name="telefone" ng-model="cad.telefone" autocomplete="off" ui-br-phone-number-mask="areaCode" required="required">
+                                        <label for="nr_telefone">Telefone:</label>
+                                        <input type="text" id="nr_telefone" name="nr_telefone" ng-model="cad.nr_telefone" autocomplete="off" ui-br-phone-number-mask="areaCode">
                                     </div>
                                     <div class="col-sm-6">
-                                        <label for="celular">Celular:</label>
-                                        <input type="text" id="celular" name="celular" ng-model="cad.celular" autocomplete="off" ui-br-phone-number-mask="areaCode" required="required">
+                                        <label for="nr_contato">Celular:</label>
+                                        <input type="text" id="nr_contato" name="nr_contato" ng-model="cad.nr_contato" autocomplete="off" ui-br-phone-number-mask="areaCode">
                                     </div>
                                 </div>
                             </div>
@@ -76,40 +85,36 @@
                             <div class="corpo-painel">
                                 <div class="row">
                                     <div class="col-sm-6">
-                                        <label for="cep">CEP:</label>
-                                        <input type="text" ui-mask="99999-999" name="cep" autocomplete="off" ng-model="cad.cep">
+                                        <label for="nr_cep">CEP:</label>
+                                        <input type="text" ui-mask="99999-999" name="nr_cep" autocomplete="off" ng-model="cad.nr_cep" required="required">
                                     </div>
                                     <div class="col-sm-6">
                                         <label for="cidade">Cidade:</label>
-                                        <input type="text" name="cidade" autocomplete="off">
+                                        <select name="cd_cidade" id="cd_cidade" autocomplete="off" ng-model="cad.cd_cidade" required="required">
+                                            <option value="1">Criciúma</option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-3">
-                                        <label for="numero">Número:</label>
-                                        <input type="text" name="numero" autocomplete="off">
+                                        <label for="nr_endereco">Número:</label>
+                                        <input type="text" name="nr_endereco" autocomplete="off" ng-model="cad.nr_endereco" required="required" maxlength="5">
                                     </div>
                                     <div class="col-sm-9">
-                                        <label for="logradouro">Logradouro:</label>
-                                        <input type="text" name="logradouro" autocomplete="off">
+                                        <label for="ed_cadastro">Endereço:</label>
+                                        <input type="text" name="ed_cadastro" autocomplete="off" ng-model="cad.ed_cadastro" required="required" maxlength="80">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        <label for="endereco">Endereço:</label>
-                                        <input type="text" name="endereco" autocomplete="off">
+                                        <label for="ba_cadastro">Bairro:</label>
+                                        <input type="text" name="ba_cadastro" autocomplete="off" ng-model="cad.ba_cadastro" required="required" maxlength="50">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        <label for="bairro">Bairro:</label>
-                                        <input type="text" name="bairro" autocomplete="off">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <label for="complemento">Complemento:</label>
-                                        <input type="text" name="complemento" autocomplete="off">
+                                        <label for="co_complemento">Complemento:</label>
+                                        <input type="text" name="co_complemento" autocomplete="off" ng-model="cad.co_complemento" maxlength="80">
                                     </div>
                                 </div>
                             </div>
@@ -121,25 +126,25 @@
                             <div class="corpo-painel">
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        <label for="email">E-mail:</label>
-                                        <input type="email" name="email" autocomplete="off">
+                                        <label for="ed_email">E-mail:</label>
+                                        <input type="email" name="ed_email" autocomplete="off" ng-model="cad.ed_email" required="required" maxlength="80">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
                                         <label for="senha">Senha:</label>
-                                        <input type="password" name="senha" autocomplete="off">
+                                        <input type="password" name="senha" autocomplete="off" ng-model="cad.senha" required="required" maxlength="20">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
                                         <label for="confirm_senha">Confirmar Senha:</label>
-                                        <input type="password" name="confirm_senha" autocomplete="off">
+                                        <input type="password" name="confirm_senha" autocomplete="off" ng-model="cad.confirm_senha" required="required" maxlength="20">
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <input type="submit" id="submit" class="clearfix btn" value="Cadastrar">
+                        <button type="submit" id="submit" class="clearfix btn">Cadastrar</button>
                     </div>
                 </form>
             </div>
